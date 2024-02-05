35e5fb121d581c7b0c924b1036be0a8ac59946eb
DjMarte <alphat098@gmail.com>
2024-02-05 15:29:31 -0400

    Cree la clase "Canaste de Compra"

---
+++ Pizza_Place/PizzaPlace/CanastaDeCompra.cs
@@ -0,0 +1,9 @@
+﻿namespace Pizza_Place.PizzaPlace
+{
+    public class CanastaDeCompra
+    {
+        public Cliente cliente { get; set; } = new Cliente();
+        public List<int> Pedidos { get; set; } = new List<int>();
+        public bool HaPagado { get; set; }
+    }
+}
