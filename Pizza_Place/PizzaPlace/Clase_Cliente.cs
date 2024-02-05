6d83ff15ad7fd944a4cd37175e29321df8aa7269
DjMarte <alphat098@gmail.com>
2024-02-05 15:13:31 -0400

    Creación de la clase cliente

---
+++ Pizza_Place/PizzaPlace/Clase_Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace Pizza_Place.PizzaPlace
+{
+    public class Clase_Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = default!;
+        public string Calle { get; set; } = default!;
+        public string Ciudad { get; set; } = default!;
+    }
+}
